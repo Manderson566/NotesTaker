7ad8e612e451d0b6f86c5bfe10e2019210a4d4ac
Michael Anderson <Micmail566@gmail.com>
2017-02-02 10:19:08 -0500

    Got notes to show on Default form.

--- NotesTaker/Default.aspx.cs
+++ NotesTaker/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NotesTaker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,13 @@ namespace NotesTaker
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Note> Notes;
         protected void Page_Load(object sender, EventArgs e)
         {
+            using (var db = new NoteContext())
+            {
+                Notes = db.Notes.ToList();
+            }
 
         }
     }
